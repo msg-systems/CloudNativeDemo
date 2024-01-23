c87acfebdab0f2eb6e463a3fa30b733ef7476bc4
JuergenKahl <103061017+JuergenKahl@users.noreply.github.com>
2024-01-23 18:40:51 +0100

    Update main.tf
    New postgres version / commented out acl on S3 bucket

--- terraform/main.tf
+++ terraform/main.tf
@@ -23,10 +23,10 @@ resource "aws_s3_bucket" "this" {
   force_destroy = true # delete all bucket resources on destroy so bucket can be removed
 }
 
-resource "aws_s3_bucket_acl" "this" {
-  bucket = aws_s3_bucket.this.id
-  acl    = "private"
-}
+#resource "aws_s3_bucket_acl" "this" {
+#  bucket = aws_s3_bucket.this.id
+#  acl    = "private"
+#}
 
 resource "aws_s3_bucket_server_side_encryption_configuration" "this" {
   bucket = aws_s3_bucket.this.id
@@ -554,7 +554,7 @@ module "db" {
   identifier = "${var.app_repo_name}-db"
 
   engine            = "postgres"
-  engine_version    = "13.3"
+  engine_version    = "15.5"
   instance_class    = "db.t3.micro"
   allocated_storage = 5
 
@@ -577,9 +577,9 @@ module "db" {
   skip_final_snapshot = true
 
   # DB parameter group
-  family = "postgres13"
+  family = "postgres15"
   # DB option group
-  major_engine_version      = "13"
+  major_engine_version      = "15"
   create_db_option_group    = false
   create_db_parameter_group = true
 
